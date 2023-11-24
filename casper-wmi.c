58656da55ffa3b62bddec5178cd08e1d0e7e3534
Mustafa Ekşi <mustafa.eskieksi@gmail.com>
2023-11-25 00:03:20 +0300

    Current ver: trying to use multicolor class...

--- casper-wmi.c
+++ casper-wmi.c
@@ -1,34 +1,78 @@
-#include <linux/kernel.h>
-#include <linux/module.h>
-#include <linux/printk.h>
-#include <linux/init.h>
 #include <linux/acpi.h>
+#include <linux/leds.h>
+#include <linux/slab.h>
+#include <linux/module.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Mustafa Ekşi");
 MODULE_DESCRIPTION(
 	"A basic WMI driver for Casper Excalibur Laptop's keyboard backlights.");
+	
+#define CASPER_LED_BIOS_GUID "644C5791-B7B0-4123-A90B-E93876E0DAAD"
 
-#define CASPER_EXCALIBUR_WMI_OBJECT_GUID "644C5791-B7B0-4123-A90B-E93876E0DAAD"
-
-static int __init load_module(void) 
+static int casper_led_perform_fn(u8 region, u8 red, u8 green,
+			       u8 blue, u8 alpha)
 {
-	if (!wmi_has_guid(CASPER_EXCALIBUR_WMI_OBJECT_GUID)) {
-	    printk("No known WMI GUID found\n");
-	    return -ENODEV;
+	struct acpi_buffer output = { ACPI_ALLOCATE_BUFFER, NULL };
+	struct bios_args *bios_return;
+	struct acpi_buffer input;
+	union acpi_object *obj;
+	acpi_status status;
+	u8 return_code;
+
+	struct bios_args args = {
+		.length = length,
+		.result_code = result_code,
+		.device_id = device_id,
+		.command = command,
+		.on_time = on_time,
+		.off_time = off_time
+	};
+
+	input.length = sizeof(struct bios_args);
+	input.pointer = &args;
+
+	status = wmi_evaluate_method(DELL_LED_BIOS_GUID, 0, 1, &input, &output);
+	if (ACPI_FAILURE(status))
+		return status;
+
+	obj = output.pointer;
+
+	if (!obj)
+		return -EINVAL;
+	if (obj->type != ACPI_TYPE_BUFFER) {
+		kfree(obj);
+		return -EINVAL;
 	}
-	char magical_input[32] = {0x00,0xfb,0x00,0x01,0x06,0x00,0x00,0x00,0x11,0x40,0xff,0x11,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00};
-	struct acpi_buffer input = {(acpi_size)sizeof(char)*32, &magical_input};
-	//struct acpi_buffer output = { ACPI_ALLOCATE_BUFFER, NULL };
-	wmi_set_block(CASPER_EXCALIBUR_WMI_OBJECT_GUID, 0,
-					&input);
-	return 0;
-} 
 
-void unload_module(void) 
-{ 
-	pr_info("Goodbye world.\n"); 
-} 
+	bios_return = ((struct bios_args *)obj->buffer.pointer);
+	return_code = bios_return->result_code;
 
-module_init(load_module); 
-module_exit(unload_module);
+	kfree(obj);
+
+	return return_code;
+}
+
+static int __init casper_led_init(void)
+{
+	int error = 0;
+
+	if (!wmi_has_guid(CASPER_LED_BIOS_GUID))
+		return -ENODEV;
+
+	error = led_off();
+	if (error != 0)
+		return -ENODEV;
+
+	return led_classdev_register(NULL, &dell_led);
+}
+
+static void __exit casper_led_exit(void)
+{
+	led_classdev_unregister(&dell_led);
+
+	led_off();
+}
+
+module_init(casper_led_init); 
+module_exit(casper_led_exit);
