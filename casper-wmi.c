a0d283af217a0b9add9daa25f8076e927fd2f758
Mustafa Ekşi <mustafa.eskieksi@gmail.com>
2023-11-27 23:30:35 +0300

    Minor adjustments

--- casper-wmi.c
+++ casper-wmi.c
@@ -4,19 +4,15 @@
 #include <linux/module.h>
 #include <linux/wmi.h>
 #include <linux/device.h>
-
-MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Mustafa Ekşi");
-MODULE_DESCRIPTION(
-	"A basic WMI driver for Casper Excalibur Laptop's keyboard backlights.");
+#include <linux/dev_printk.h>
 	
-#define CASPER_LED_BIOS_GUID "644C5791-B7B0-4123-A90B-E93876E0DAAD"
+#define CASPER_EXCALIBUR_WMI_GUID "644C5791-B7B0-4123-A90B-E93876E0DAAD"
 
-struct zone_data {
+struct casper_zone_data {
 	u8 r, g, b, a;
 };
 
-struct zone_data casper_wmi_data[4] = {0};
+struct casper_zone_data casper_wmi_data[4] = {0};
 
 static acpi_status casper_set_single_zone(size_t zone_id) {
 	char magical_input[32] = {
@@ -31,12 +27,11 @@ static acpi_status casper_set_single_zone(size_t zone_id) {
 		0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00
 	};
 	struct acpi_buffer input = {(acpi_size)sizeof(char)*32, &magical_input};
-	//struct acpi_buffer output = { ACPI_ALLOCATE_BUFFER, NULL };
-	return wmi_set_block(CASPER_LED_BIOS_GUID, 0,
+	return wmi_set_block(CASPER_EXCALIBUR_WMI_GUID, 0,
 					&input);
 }
 
-static acpi_status casper_perform_wmi(void) {
+static acpi_status casper_send_zones(void) {
 	for(size_t i = 0; i<4; i++) {
 		acpi_status ret = casper_set_single_zone(i);
 		if(ret != 0)
@@ -64,16 +59,14 @@ static acpi_status casper_set_all_keyboard(u8 r, u8 g, u8 b, u8 a) {
 		0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00
 	};
 	struct acpi_buffer input = {(acpi_size)sizeof(char)*32, &magical_input};
-	//struct acpi_buffer output = { ACPI_ALLOCATE_BUFFER, NULL };
-	return wmi_set_block(CASPER_LED_BIOS_GUID, 0,
+	return wmi_set_block(CASPER_EXCALIBUR_WMI_GUID, 0,
 					&input);
 }
 
 static ssize_t zone_colors_show(struct device *dev, struct device_attribute *attr,
 			        char *buf)
 {
-	printk("zone_colors_show\n");
-	int count = sprintf(buf, "zone 0: #%02x%02x%02x%02x\nzone 1: #%02x%02x%02x%02x\nzone 2: #%02x%02x%02x%02x\nzone 3: #%02x%02x%02x%02x\n",
+	return sprintf(buf, "zone 0: #%02x%02x%02x%02x\nzone 1: #%02x%02x%02x%02x\nzone 2: #%02x%02x%02x%02x\nzone 3: #%02x%02x%02x%02x\n",
 		casper_wmi_data[0].r, casper_wmi_data[0].g,
 		casper_wmi_data[0].b, casper_wmi_data[0].a,
 		
@@ -86,10 +79,6 @@ static ssize_t zone_colors_show(struct device *dev, struct device_attribute *att
 		casper_wmi_data[3].r, casper_wmi_data[3].g,
 		casper_wmi_data[3].b, casper_wmi_data[3].a
 	);
-	printk("%d\n", count);
-	//char *hello = "hello";
-	//strncpy(buf, hello, strlen(hello));
-	return count;
 }
 
 static ssize_t zone_colors_store(struct device *dev, struct device_attribute *attr,
@@ -104,8 +93,8 @@ static ssize_t zone_colors_store(struct device *dev, struct device_attribute *at
 	
 	u8 zone = (tmp>>(8*4))&0xFF;
 	if(zone > 4) {
-		printk("Zone doesn't exist\n");
-		return 1;
+		dev_err(dev, "zone_colors_store: this zone doesn't exist\n");
+		return -1;
 	}
 	printk("ret a: %0x\n",(u8) tmp&0xFF);
 	if(zone == 4) {
@@ -117,7 +106,8 @@ static ssize_t zone_colors_store(struct device *dev, struct device_attribute *at
 			(u8) tmp&0xFF
 		);
 		if(ret != 0) {
-			printk("ACPI status: %d\n", ret);
+			dev_err(dev, "ACPI status: %d\n", ret);
+			return ret;
 		}
 	}else {
 		casper_wmi_data[zone].a = (u8) tmp&0xFF;
@@ -126,7 +116,8 @@ static ssize_t zone_colors_store(struct device *dev, struct device_attribute *at
 		casper_wmi_data[zone].b = (u8) (tmp>>8)&0xFF;
 		ret = casper_set_single_zone(zone);
 		if(ret != 0) {
-			printk("ACPI status: %d\n", ret);
+			dev_err(dev, "ACPI status: %d\n", ret);
+			return ret;
 		}
 	}	
 	
@@ -139,11 +130,8 @@ static struct attribute *casper_kbd_led_attrs[] = {
 	&dev_attr_zone_colors.attr,
 	NULL,
 };
-ATTRIBUTE_GROUPS(casper_kbd_led);
 
-struct casper_kbd_backlights_data {
-	u8 zone, red, green, blue, alpha_and_pattern;
-};
+ATTRIBUTE_GROUPS(casper_kbd_led);
 
 void set_casper_backlight_brightness(struct led_classdev *led_cdev,
 					  enum led_brightness brightness)
@@ -152,20 +140,19 @@ void set_casper_backlight_brightness(struct led_classdev *led_cdev,
 	casper_wmi_data[1].a = (casper_wmi_data[1].a&0xF0) | (u8)brightness;
 	casper_wmi_data[2].a = (casper_wmi_data[2].a&0xF0) | (u8)brightness;
 	casper_wmi_data[3].a = (casper_wmi_data[3].a&0xF0) | (u8)brightness;
+	
 	acpi_status ret = casper_set_single_zone(0);
-	if(ret != 0) {
-		printk("casper_perform_wmi returned non zero value\n");
-	}
+	if(ret != 0)
+		dev_err(led_cdev->dev, "Couldn't set brightness acpi status: %d\n", ret);
+	
 	ret = casper_set_single_zone(1);
-	if(ret != 0) {
-		printk("casper_perform_wmi returned non zero value\n");
-	}
-	printk("set_brightness. brightness: %d\n", brightness);
+	if(ret != 0)
+		dev_err(led_cdev->dev, "casper_perform_wmi returned non zero value\n");
 }
 
-enum led_brightness get_casper_backlight_brightness(struct led_classdev *led_cdev)
+enum led_brightness get_casper_backlight_brightness(
+				struct led_classdev *led_cdev)
 {
-	//printk("a: %d\n", casper_wmi_data[3].a);
 	u8 brightness = casper_wmi_data[3].a&0x0F;
 	if(brightness == 0)
 		return 0;
@@ -186,11 +173,11 @@ static struct led_classdev casper_kbd_led = {
 
 static int __init casper_led_init(void)
 {
-	if (!wmi_has_guid(CASPER_LED_BIOS_GUID))
+	if (!wmi_has_guid(CASPER_EXCALIBUR_WMI_GUID))
 		return -ENODEV;
-	
-	if(casper_perform_wmi() != 0) {
-		printk("casper_perform_wmi returned non zero value\n");
+	// Set all keyboard zones to zero and off because 
+	if(casper_send_zones() != 0) {
+		dev_err(NULL, "casper_perform_wmi returned non zero value\n");
 	}
 	return led_classdev_register(NULL, &casper_kbd_led);
 }
@@ -202,3 +189,7 @@ static void __exit casper_led_exit(void)
 
 module_init(casper_led_init); 
 module_exit(casper_led_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Mustafa Ekşi");
+MODULE_DESCRIPTION("Casper Excalibur Laptops' keyboard backlight driver");
